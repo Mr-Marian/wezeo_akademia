f2098feef0cfc028e954b0a92319f9e6f70849db
Marian Kapitan <145431830+Mr-Marian@users.noreply.github.com>
2025-08-20 01:20:14 +0800

    pridal som tailwind a zacal s upravami css pomocou tailwindu, zatial iba header a ciastocne nav

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", 
+    "./public/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#3597ff',
+        'secondary': '#f3f9ff',
+        'txt': {
+          100: '#fff',
+          400: '#9b9ca2',
+          500: '#566b8d',
+          600: '#1a232f',
+        },
+        'status-blue': '#5b94d7',
+        'status-green': '#5cc19a',
+        'status-yellow': '#dbca68',
+        'status-red': '#b95e73',
+        'status-blue-background': 'rgba(221, 236, 251, 0.4)',
+        'status-green-background': 'rgba(156, 238, 204, 0.4)',
+        'status-yellow-background': 'rgba(238, 226, 156, 0.4)',
+        'status-red-background': 'rgba(238, 156, 156, 0.4)',
+        'my-bg': {
+          100: '#f0f0f1',
+          200: '#f3f9ff',
+          300: '#d7eaff',
+        },
+        'arrow': '#b8c0cf',
+        'shadow': '#f7f8fa',
+        //border colors
+        'button': '#e3e4e6',
+        'header': '#d7eaff',
+      },
+      minWidth: {
+        '40': '160px',
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+  ],
+}
+
